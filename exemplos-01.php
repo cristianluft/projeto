bdfc4daf76da853646e4162eec53a2b66a187aab
Cristian Luft <cristian-luft@hotmail.com>
2020-05-03 12:17:37 -0300

    o que foi visto até aula 15

---
+++ exemplos-01.php
@@ -0,0 +1,26 @@
+<?php
+
+$ano = 1997;
+$nome = "Cristian";
+$sobrenome = 'Luft';
+$salario = 5500.99;
+$bloqueado = false;
+////////////////////////
+$frutas = array("abacaxi","laranja","manga");
+
+///echo $frutas[2];
+
+$nascimento = new DateTime();
+
+//var_dump($nascimento);
+///////////////////////////////////
+
+$arquivo = fopen("exemplos-01.php", "r");
+
+//var_dump($arquivo);
+
+$nulo = NULL;
+$vazio = "";
+
+
+?> 
---
+++ exemplos-02.php
@@ -0,0 +1,9 @@
+<?php
+
+$nome = (int)$_GET["a"];
+$ano = (int)$_GET["b"];
+$ip = $_SERVER["SCRIPT_NAME"];
+
+echo ($ip);
+
+?>
